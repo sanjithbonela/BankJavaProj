14d7e862a6599fd879292fbebd23b482bcefd699
San <sanjithbonela@gmail.com>
2018-08-18 11:06:06 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+package otp;
+import java.awt.BorderLayout;
+
+import javax.swing.Box;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Main {
+  public static void main(String args[]) {
+    JFrame f = new JFrame("JPasswordField Sample");
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Box rowOne = Box.createHorizontalBox();
+    rowOne.add(new JLabel("Username"));
+    rowOne.add(new JTextField());
+    Box rowTwo = Box.createHorizontalBox();
+    rowTwo.add(new JLabel("Password"));
+    JPasswordField jpassword = new JPasswordField();
+    
+    rowTwo.add(jpassword);
+    f.add(rowOne, BorderLayout.NORTH);
+    f.add(rowTwo, BorderLayout.SOUTH);
+    f.setSize(300, 200);
+    f.setVisible(true);
+    
+    jpassword.setEchoChar('*');
+    System.out.println(jpassword.getEchoChar());
+    
+  }
+}
